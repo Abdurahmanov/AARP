908ac6ca9e84c343674d3c5d5b79194a2de29a47
artur <abdurahmanov.art@gmail.com>
2017-08-26 19:14:27 +0000

    final

--- js/script.js
+++ js/script.js
@@ -1,20 +1,9 @@
 $(function() {
-    $('.media__photo-block').owlCarousel({
-        margin:0,
-        loop:true,
-        autoWidth:true,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-            },
-            600:{
-                items:3
-            },
-            1000:{
-                items:6
-            }
-        }
+    $('.header__burger').click(function () {
+        $('.page').addClass('menu_active')
     })
 
+    $('.header__close').click(function () {
+        $('.page').removeClass('menu_active')
+    })
 });
